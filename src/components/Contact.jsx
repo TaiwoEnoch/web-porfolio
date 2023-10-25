62c2ea733c7561c6690571fbef85b1c20815e6f3
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-10-25 21:28:37 +0100

    Make use of Emailjs for form validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,102 +1,100 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { motion } from 'framer-motion';
+import emailjs from 'emailjs-com';
+
+import { fadeIn, textVariant } from '../utils/motion';
 import SectionWrapper from '../hoc';
-import { fadeIn, textVariant, slideIn } from '../utils/motion';
-import contact from '../assets/contact.png';
 import style from './styles/contact.module.css';
 
-const Contact = () => {
-  const [form, setForm] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const formRef = useRef(null);
 
-  const handleChange = (e) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
     const { target } = e;
-    const { name, value } = target;
 
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const isValid = form.checkValidity();
-    if (isValid) {
-      // Submit the form
-      form.submit();
-      setForm({
-        name: '',
-        email: '',
-        message: '',
-      });
+    try {
+      await emailjs.sendForm(
+        'service_57i5icy',
+        'template_n9fjqe7',
+        target,
+        'vM42l9I5NAfsr4M0d',
+      );
+      setSuccessMessage('Your message has been sent!');
+      setName('');
+      setEmail('');
+      setMessage('');
+      setTimeout(() => {
+        setSuccessMessage('');
+      }, 3000);
+    } catch (error) {
+      setErrorMessage('Oops! Something went wrong. Please try again later.');
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 3000);
     }
   };
 
   return (
-    <>
-      <motion.h1 variants={textVariant()} className={style.title}>
-        Get in Touch!
-      </motion.h1>
-      <motion.p variants={fadeIn('', '', 0.15, 1)} className={style.subtitle}>
-        I&apos;m always excited to hear about new opportunities and collaborations. Don&apos;t hesitate to reach out and let&apos;s make something great.
-      </motion.p>
-      <div className={style.container}>
-        <motion.form
-          variants={slideIn('left', '', 0, 1)}
-          onSubmit={handleSubmit}
-          action="https://formspree.io/f/xjvdkoyw"
-          method="post"
-          className={style.form_container}
-        >
+    <div className="form-container">
+      <form className="form" onSubmit={handleSubmit} ref={formRef}>
+        <motion.h1 variants={textVariant()} className={style.title}>
+          Get in Touch!
+        </motion.h1>
+        <motion.p variants={fadeIn('', '', 0.15, 1)} className={style.subtitle}>
+          It will be a pleasure to hear from you! If you have an application you are interested in developing, a feature that you need built or a project that needs coding. I&apos;d love to help with it.
+        </motion.p>
+        {successMessage && <p className="success-message">{successMessage}</p>}
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        <div className={style.container}>
           <div className={style.form}>
             <h4 className={style.form_title}>Contact Me</h4>
             <input
               type="text"
-              placeholder="Name"
-              required
-              maxLength="30"
               name="name"
-              value={form.name}
-              onChange={handleChange}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Your Name"
               className={style.input}
+              required
             />
             <input
               type="email"
-              placeholder="Email"
-              required
               name="email"
-              value={form.email}
-              onChange={handleChange}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Your Email"
+              required
               className={style.input}
             />
             <textarea
               cols="30"
               rows="10"
               placeholder="Your message"
-              required
               maxLength="500"
               name="message"
-              value={form.message}
-              onChange={handleChange}
               className={`${style.input} ${style.textarea}`}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
             />
-            <button type="submit" className={style.btn_container}>
-              <span className={style.btn_hover}>Get in touch</span>
-              <span className={style.btn}>Get in touch</span>
+            <button
+              type="submit"
+              className={style.btn_container}
+            >
+              Send Message
             </button>
           </div>
-        </motion.form>
-        <motion.div variants={slideIn('right', '', 0, 1)} className={style.img_container}>
-          <img src={contact} alt="contact" className={style.img} loading="lazy" />
-        </motion.div>
-      </div>
-    </>
+        </div>
+      </form>
+    </div>
   );
 };
 
-export default SectionWrapper(Contact, 'contact', 'my-0');
+export default SectionWrapper(ContactForm, 'contact', 'my-0');
